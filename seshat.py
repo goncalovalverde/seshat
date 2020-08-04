d5ad897bf8de9c9d22af24eea8e3ae2eabeb264c
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-08-04 11:31:40 +0100

    Got jira working

--- seshat.py
+++ seshat.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python
 import yaml
+import pprint
+from atlassian import Jira
 
 config_file=open("config.yml")
 config=yaml.load(config_file,Loader=yaml.FullLoader)
 
-print(config["jira"]["username"])
+
+def get_cycle(issue):
+    print("-----")
+    print(issue["key"])
+    pprint.pprint(issue["fields"]["created"])
+
+    for history in issue["changelog"]["histories"]:
+        for item in history["items"]:
+            if item["field"] == 'status':
+                print('Date:' + history["created"] + ' From:' + item["fromString"] + ' To:' + item["toString"])
+#                pprint.pprint(item)
+
+jira = Jira(
+    url=config["jira"]["url"],
+    username=config["jira"]["username"],
+    password=config["jira"]["password"])
+
+issues = jira.jql(config["jql_query"],expand='changelog')
+
+#pprint.pprint(issues["issues"])
+
+for issue in issues["issues"]:
+    get_cycle(issue)
