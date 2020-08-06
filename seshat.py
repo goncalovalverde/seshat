818d76b042d52bb48d593e92fe8f8b940a6cc523
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-08-06 22:40:45 +0100

    Added reading csv file

--- seshat.py
+++ seshat.py
@@ -2,9 +2,13 @@
 import yaml
 import pprint
 from atlassian import Jira
+import pandas as pd
+import dateutil.parser
 
 config_file=open("config.yml")
 config=yaml.load(config_file,Loader=yaml.FullLoader)
+mode=config["input"]["mode"]
+csv_file=config["input"]["csv_file"]
 
 
 def get_cycle(issue):
@@ -18,14 +22,20 @@ def get_cycle(issue):
                 print('Date:' + history["created"] + ' From:' + item["fromString"] + ' To:' + item["toString"])
 #                pprint.pprint(item)
 
-jira = Jira(
-    url=config["jira"]["url"],
-    username=config["jira"]["username"],
-    password=config["jira"]["password"])
-
-issues = jira.jql(config["jql_query"],expand='changelog')
 
 #pprint.pprint(issues["issues"])
 
-for issue in issues["issues"]:
-    get_cycle(issue)
+if mode == "csv":
+    cycle=pd.read_csv(csv_file)
+elif mode == "jira":
+    jira = Jira(
+        url=config["jira"]["url"],
+        username=config["jira"]["username"],
+        password=config["jira"]["password"])
+
+    issues = jira.jql(config["jql_query"],expand='changelog')
+
+    for issue in issues["issues"]:
+        get_cycle(issue)
+
+print(cycle)
