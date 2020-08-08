c8c9b7a4e144d2ad6ff4fb1b511e34c5403176cd
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-08-08 06:35:48 +0100

    Moved jira info to a separate package

---
+++ reader/jira.py
@@ -0,0 +1,49 @@
+from jira import JIRA
+import dateutil.parser
+
+def get_issue_data(issue,issue_data,config):
+    issue_data["Key"].append(issue.key)
+    issue_data["Type"].append(issue.fields.issuetype.name)
+    issue_data["Created"].append(dateutil.parser.parse(issue.fields.created))
+
+    history_item={}
+    for workflow_step in config["Workflow"]:
+        history_item[workflow_step]="0"
+
+    for history in issue.changelog.histories:
+        for item in history.items:
+            if item.field == 'status':
+                history_item[item.toString]=dateutil.parser.parse(history.created)
+    
+    for workflow_step in config["Workflow"]:
+        issue_data[workflow_step].append(history_item[workflow_step])
+
+def get_issues(config):
+    jira = JIRA(
+        config["jira"]["url"],
+        basic_auth=(config["jira"]["username"],config["jira"]["password"])
+    )
+
+    query_options = {
+        "expand": 'changelog',
+        "max_results": 100
+    }
+    issues = jira.search_issues(config["jql_query"],expand="changelog")
+    return issues
+
+def get_jira_data(config):
+    issue_data={
+        "Key": [],
+        "Type": [], 
+        "Created": []
+    }
+
+    for workflow_step in config["Workflow"]:
+        issue_data[workflow_step]=[]
+
+    issues=get_issues(config)    
+
+    for issue in issues:
+        get_issue_data(issue,issue_data,config)
+
+    return issue_data
--- seshat.py
+++ seshat.py
@@ -1,65 +1,18 @@
 #!/usr/bin/python
 import yaml
 import pprint
-from jira import JIRA
 import pandas as pd
-import dateutil.parser
+import reader.jira
 
 config_file=open("config.yml")
 config=yaml.load(config_file,Loader=yaml.FullLoader)
 mode=config["input"]["mode"]
 
-def get_issue_data(issue,issue_data):
-    issue_data["Key"].append(issue.key)
-    issue_data["Type"].append(issue.fields.issuetype.name)
-    issue_data["Created"].append(dateutil.parser.parse(issue.fields.created))
-
-    history_item={}
-    for workflow_step in config["Workflow"]:
-        history_item[workflow_step]="0"
-
-    for history in issue.changelog.histories:
-        for item in history.items:
-            if item.field == 'status':
-                history_item[item.toString]=dateutil.parser.parse(history.created)
-    
-    for workflow_step in config["Workflow"]:
-        issue_data[workflow_step].append(history_item[workflow_step])
-
-def get_issues():
-    jira = JIRA(
-        config["jira"]["url"],
-        basic_auth=(config["jira"]["username"],config["jira"]["password"])
-    )
-
-    query_options = {
-        "expand": 'changelog',
-        "max_results": 100
-    }
-    issues = jira.search_issues(config["jql_query"],expand="changelog")
-    return issues
-
-def get_jira_data():
-    issue_data={
-        "Key": [],
-        "Type": [], 
-        "Created": []
-    }
-
-    for workflow_step in config["Workflow"]:
-        issue_data[workflow_step]=[]
-
-    issues=get_issues()    
-
-    for issue in issues:
-        get_issue_data(issue,issue_data)
-
-    return issue_data
 
 if mode == "csv":
     csv_file=config["input"]["csv_file"]
     cycle=pd.read_csv(csv_file)
 elif mode == "jira":
-    cycle=pd.DataFrame(get_jira_data())
+    cycle=pd.DataFrame(reader.jira.get_jira_data(config))
 
 print(cycle)
