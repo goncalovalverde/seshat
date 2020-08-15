6b07c519a51f6cbe762427ca256972edecf6bce5
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-08-15 18:57:28 +0100

    Converting Created date to datetime

--- reader/csv.py
+++ reader/csv.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def read_csv(csv_file,workflow):
     cycle_data=pd.read_csv(csv_file)
+    cycle_data["Created"]=pd.to_datetime(cycle_data["Created"])
     for item in workflow:
         if item in cycle_data:
             cycle_data[item]=pd.to_datetime(cycle_data[item])
