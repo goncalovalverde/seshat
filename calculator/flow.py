d47a83584181b43116be0932e007514881275d09
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-08-15 11:03:55 +0100

    Flow is now calculated by week number

--- calculator/flow.py
+++ calculator/flow.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-def throughput(cycle):
-    table=pd.pivot_table(cycle,values="Key",index=['Done'],columns="Type",aggfunc='count')
-    return table
+def throughput(cycle_data):
+    # We need to pivot data since several values in the Done column will be nill or 0
+    table=pd.pivot_table(cycle_data,values="Key",index=['Done'],columns="Type",aggfunc='count')
+    throughput = pd.DataFrame(table.to_records())
+    throughput["Week"] = throughput["Done"].dt.strftime('%Y-%U')
+    throughput = throughput.rename({"Done":"Total"},axis=1)
+    throughput = throughput.groupby("Week").count()
+    return throughput
