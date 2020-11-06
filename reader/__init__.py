eead8564380aa48f8c1078269f6a7e4f4c56d395
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-11-06 21:04:05 +0000

    Added documentation

--- reader/__init__.py
+++ reader/__init__.py
@@ -4,16 +4,19 @@ import logging
 
 
 def read_data(config):
+    """Based in 'config' decide if we need to import data from a csv file or from jira
+    and then invoke the right method.
+    Read the information into a dataframe called 'cycle_data'
+    """
     cycle_data = None
 
     mode = config["input"]["mode"]
 
     if mode == "csv":
-        cycle_data = reader.csv.read(
-            config["input"]["csv_file"], config["Workflow"])
+        cycle_data = reader.csv.read(config["input"]["csv_file"], config["Workflow"])
     elif mode == "jira":
         jira = reader.jira.Jira(config["jira"], config["Workflow"])
-        cycle_data = jira.get_jira_data()
+        cycle_data = jira.get_data()
 
     return cycle_data
 
--- reader/csv.py
+++ reader/csv.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 
 def read(csv_file, workflow):
+    """Read information from 'csv_file' into dataframe 'cycle_data'
+    Interact over 'workflow' and convert the date into datetime
+    """
+
     cycle_data = pd.read_csv(csv_file)
     # Carefull with locale while loading the file!
     cycle_data["Created"] = pd.to_datetime(cycle_data["Created"])
