cfb3d18c4c3d4218e0cedb93e35ef1cecbbfafba
Gonçalo Valverde <goncalo.valverde@gmail.com>
2020-11-29 21:41:51 +0000

    configurable conf directory

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ def get():
     with open("config.yml", "r") as f:
         config = yaml.load(f.read(), Loader=yaml.FullLoader)
         # Add special "issue_type" Total to ensure we can see the total in all graphs
-        config["input"] = get_input()
+        config["input"] = get_input(config["config_dir"])
     return config
 
 
-def get_input():
+def get_input(config_dir):
     input = []
-    for file_name in glob.iglob("conf/*.yml"):
+    for file_name in glob.iglob(f"{config_dir}/*.yml"):
         logging.error(f"Reading input configuration from {file_name}")
         with open(file_name, "r") as f:
             config = yaml.load(f.read(), Loader=yaml.FullLoader)
