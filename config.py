c78adaf75cba34b9af5849b980f986b9046d3a66
Gonçalo Valverde <goncalo.valverde@gmail.com>
2021-01-01 08:10:08 +0000

    correct log level

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+from array import ArrayType
 import yaml
 import glob
 import logging
 
 
-def get(type="all"):
+def get(type: str = "all") -> dict:
     with open("config.yml", "r") as f:
         config = yaml.load(f.read(), Loader=yaml.FullLoader)
         if type == "all":
@@ -13,10 +14,10 @@ def get(type="all"):
             return config[type]
 
 
-def get_input(config_dir):
+def get_input(config_dir: str) -> list:
     input = []
     for file_name in sorted(glob.iglob(f"{config_dir}/*.yml")):
-        logging.error(f"Reading input configuration from {file_name}")
+        logging.info(f"Reading input configuration from {file_name}")
         with open(file_name, "r") as f:
             config = yaml.load(f.read(), Loader=yaml.FullLoader)
             input.append(config)
