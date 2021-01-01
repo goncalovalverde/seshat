f4bdb877411b4bea41f4a2f50384db6f0ebb3b40
Gonçalo Valverde <goncalo.valverde@gmail.com>
2021-01-01 16:34:30 +0000

    SES-53 : implement refresh data

--- reader/csv.py
+++ reader/csv.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import logging
 import pandas as pd
 from pandas.core.frame import DataFrame
 
@@ -13,10 +15,14 @@ class CSV:
         Interact over 'workflow' and convert the date into datetime
         """
         csv_file = self.config["file"]
-        cycle_data = pd.read_csv(csv_file)
+        csv_data = pd.read_csv(csv_file)
         # Carefull with locale while loading the file!
         for item in self.workflow:
-            if item in cycle_data:
-                cycle_data[item] = pd.to_datetime(cycle_data[item])
+            if item in csv_data:
+                csv_data[item] = pd.to_datetime(csv_data[item])
 
-        return cycle_data
+        return csv_data
+
+    def refresh_data(self, date: datetime) -> DataFrame:
+        logging.info("Refreshing CSV data")
+        return self.get_data()
